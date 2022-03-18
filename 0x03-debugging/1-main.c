d30b97007c6335b0e3d17a872e2622d7f79d8c63
Franklyn-dotcom <mabnugofranklyn678@gmail.com>
2022-03-18 19:06:38 +0100

    Prints if interger is positiv or negative

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,8 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
- * main - tests whether a function is negative or positive
- * Return:0
+ * main - causes an infinite loop
+ * Return: 0
  */
 
 int main(void)
@@ -10,15 +11,14 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-	/**
-	 * i = 0;
+	i = 0;
+	/*
 	 * 
 	 * while (i < 10)
 	 * {
 	 *	putchar(i);
 	 * }
 	 */
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
